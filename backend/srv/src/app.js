069a865e1413d49eafc35cd8251c4bdb53c20071
serant <seran.thirugnanam@gmail.com>
2019-01-27 15:17:34 -0500

    * Created api directory to include routes and object models * Added app.js and server.js

---
+++ backend/srv/src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const conversionRoutes = require('./api/routes/conversion');
+
+const mongo_endpoint = process.env.MONGO_ENDPOINT || 'mongodb://localhost:27017/expressmongo';
+
+mongoose.connect(
+    mongo_endpoint,
+    { useNewUrlParser: true }
+)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error(err));
+
+// Represents our application
+const app = express();
+
+// Attach middleware
+app.use(express.json());
+app.use('/api/conversions', conversionRoutes);
+
+app.get('/',(req, res) => res.status(200).json(
+    { message: 'PrismPDF 2019'}
+    ));
+
+app.get('/api', (req, res) => res.status(200).json(
+        { message: 'PrismPDF API 2019' }
+    ));
+
+module.exports = app;
