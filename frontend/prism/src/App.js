d20547464a4d64ada2d176ce5e900364739e343c
HanyuXi <laoxi02@gmail.com>
2019-02-03 13:52:39 -0500

    Modified the dropbox field. Change from Textbox to dropbox. Implemented axios to post request to the backend and get pageNumber array data. pageNumber.jsx component is to display at which pages the pdf file should be separate

--- frontend/prism/src/App.js
+++ frontend/prism/src/App.js
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/navBar";
 import FilesDrop from "./components/filesDropZone";
+import PageNumberDisplay from "./components/pageNumber";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageNumberArray: []
+    };
+  }
+
+  onDrop = (acceptedFiles, rejectedFiles) => {
+    //console.log(acceptedFiles);
+    const url =
+      "http://tf-ecs-chat-1180599487.us-east-1.elb.amazonaws.com/api/conversions/";
+    const data = new FormData();
+    data.append("conversionPdf", acceptedFiles[0]);
+    axios({
+      url: url,
+      data: data,
+      method: "post"
+    }).then(r => {
+      this.setState({
+        pageNumberArray: r["data"]["createdConversion"]["content"].map(
+          x => x["pageNumber"]
+        )
+      });
+      console.log(this.state["pageNumberArray"]);
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
-        <FilesDrop />
+        <FilesDrop onDrop={this.onDrop} />
+        <PageNumberDisplay {...this.state} />
       </React.Fragment>
     );
   }
