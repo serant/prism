c09d00f5c472719973bc6abffe62cd7d5e437a8f
HanyuXi <laoxi02@gmail.com>
2019-09-02 23:54:19 -0400

    feature/navbar-logo: The logo png file is under component directory and logo pic is aligned left

--- frontend/prism/src/components/navBar.jsx
+++ frontend/prism/src/components/navBar.jsx
@@ -3,7 +3,10 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1">Prism</span>
+        <span className="navbar-brand mb-0 h1">
+          Prism
+          <img src={require("./Prismlogo.png")} height="60" width="200" />
+        </span>
       </nav>
     );
   }
