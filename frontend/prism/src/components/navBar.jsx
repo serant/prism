e284317d0847005c5e8e03e3230e1223ebedb705
HanyuXi <laoxi02@gmail.com>
2019-09-08 22:50:28 -0400

    delete the prism title

--- frontend/prism/src/components/navBar.jsx
+++ frontend/prism/src/components/navBar.jsx
@@ -4,7 +4,6 @@ class NavBar extends Component {
     return (
       <nav className="navbar navbar-light bg-light">
         <span className="navbar-brand mb-0 h1">
-          Prism
           <img src={require("./Prismlogo.png")} height="60" width="200" />
         </span>
       </nav>
